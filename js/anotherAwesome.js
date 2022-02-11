ee11503dc7e91432c3af9f63e5439e5dd03024a2
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-02-11 10:09:29 +0600

    Update another todo app

--- js/anotherAwesome.js
+++ js/anotherAwesome.js
@@ -1,17 +1,20 @@
 const inputBtn = document.getElementById('inputBtn');
-
+let count = 0;
 inputBtn.addEventListener('click', function () {
+    count++;
     const inputTextField = document.getElementById('inputText');
-    const totalTaskField = document.getElementById('totalTask');
     const itemsContainer = document.getElementById('items-container');
-    const totalTask = parseInt(totalTaskField.innerText);
     const inputText = inputTextField.value;
-    const newTotalTask = totalTask + 1;
-    totalTaskField.innerText = newTotalTask;
+    const d = new Date();
+    const day = d.getDate();
+    const hour = d.getHours();
+    const minute = d.getMinutes();
+    const seconds = d.getSeconds();
     
     const tr = document.createElement('tr');
-    tr.innerHTML = `<th scope="row">${newTotalTask}</th>
+    tr.innerHTML = `<th scope="row">${count}</th>
     <td>${inputText}</td>
+    <td class="status">Date: ${day}, Time: ${hour}:${minute}:${seconds}</td>
     <td class="status">Pending</td>
     <td>
         <button class="btn btn-success btn-sm completeBtn">Complete</button>
@@ -24,35 +27,21 @@ inputBtn.addEventListener('click', function () {
     const allCompleteBtn = document.getElementsByClassName('completeBtn');
     for (const completeBtn of allCompleteBtn) {
         completeBtn.addEventListener('click', function (e) {
-
             const btn = e.target;
             btn.setAttribute('disabled', true);
-
-            const totalCompletedTaskField = document.getElementById('totalCompletedTask');
-            const totalCompletedTask = parseInt(totalCompletedTaskField.innerText);
-            const newTotalTask = totalCompletedTask + 1;
-            totalCompletedTaskField.innerText = newTotalTask;
-
             const findAllParentChild =e.target.parentNode.parentNode.childNodes;
-            const statusField = findAllParentChild[4];
+            const statusField = findAllParentChild[6];
             statusField.innerText = 'Complete';
             statusField.style.color = 'green';
 
         });
     }
 
-
     const allDeleteBtn = document.getElementsByClassName('deleteBtn');
     for (const deleteBtn of allDeleteBtn) {
         deleteBtn.addEventListener('click', function (e) {
-            const totalDeletedTaskField = document.getElementById('totalDeletedTask');
             const itemsContainer = document.getElementById('items-container');
-            const totalDeletedTask = parseInt(totalDeletedTaskField.innerText);
-            const newTotalTask = totalDeletedTask + 1;
-            totalDeletedTaskField.innerText = newTotalTask;
-
             itemsContainer.removeChild(e.target.parentNode.parentNode);
-
         });
     }
 });
